c8737443969b13e5bf6d95e7101661fcb8c5f074
Smartin Developer <coop@smartinsistem.com>
2023-11-03 10:48:34 +0700

    feat: Fix Navbar

--- resources/views/students/partials/navbar.blade.php
+++ resources/views/students/partials/navbar.blade.php
@@ -5,11 +5,9 @@
   </a>
 </li>
 
-@can('lessons.index')
 <li class="nav-main-item">
   <a class="nav-main-link {{ Request::is('students/lessons*') ? 'active' : '' }}" href="{{ route('students.lessons.index') }}">
     <i class="nav-main-link-icon fa fa-folder-open"></i>
     <span class="nav-main-link-name">{{ trans('page.lessons.title') }}</span>
   </a>
 </li>
-@endcan
